f777705c34f2d9ffabc17b70928408119d962300
MarcosCouttulenc <mcouttulenc@fi.uba.ar>
2025-08-22 17:08:20 -0300

    logs incorporados

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,17 @@
 import os
+import logging
+import sys
 
-print("Hello, World!")
-print(f"Backend docker port: {os.environ.get('BACKEND_DOCKER_PORT', 'no port set')}")
-print(f"Backend exported port: {os.environ.get('BACKEND_EXPOSED_PORT', 'no port set')}")
+# --- Configuración de Logging ---
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler("app.log", encoding="utf-8"),
+        logging.StreamHandler(sys.stdout),
+    ],
+    format='%(asctime)s - %(levelname)s - %(module)s:%(lineno)d - %(message)s'
+)
+
+logging.info("Hello, World!")
+logging.info(f"Backend docker port: {os.environ.get('BACKEND_DOCKER_PORT', 'no port set')}")
+logging.info(f"Backend exported port: {os.environ.get('BACKEND_EXPOSED_PORT', 'no port set')}")
